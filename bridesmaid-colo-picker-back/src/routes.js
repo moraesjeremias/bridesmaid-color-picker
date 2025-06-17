27d0597fb5a72f532f79bd2654b359d670b5a87d
Victor <carvalho.victorfa@gmail.com>
2025-06-17 00:00:03 -0300

    feat: add chooseColor endpoint

--- bridesmaid-colo-picker-back/src/routes.js
+++ bridesmaid-colo-picker-back/src/routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express"; 
 import { colorsController } from "./controllers/colorsController.js";
+
 const routes = Router();
 
-
 routes.get("/colors", colorsController.avaiableColors);
-
 routes.post("/colors", colorsController.chooseColor);
 
 
