1ad80068c5bb50fe9650c962787d3bd7448c6dae
Jeremy <jeremias@availproject.org>
2025-06-17 21:57:23 -0300

    feat: Add CORS

--- bridesmaid-colo-picker-back/src/app.js
+++ bridesmaid-colo-picker-back/src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import {routes}  from './routes.js';
 
 export const app = express();
-
+app.use(cors())
 app.use(express.json());
+
 app.use(routes);
