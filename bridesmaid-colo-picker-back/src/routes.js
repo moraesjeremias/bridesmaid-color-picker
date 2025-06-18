77e7d635d78058e945973b69dfae5f89be072c2f
Jeremy <jeremias@availproject.org>
2025-06-18 01:23:26 -0300

    feat: adds users routes/svcs

--- bridesmaid-colo-picker-back/src/routes.js
+++ bridesmaid-colo-picker-back/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"; 
 import { colorsController } from "./controllers/colorsController.js";
+import { userController } from "./controllers/usersController.js";
 
 const routes = Router();
 
 routes.get("/colors", colorsController.getColors);
 routes.post("/colors", colorsController.chooseColor);
-
+routes.get("/users/:id", userController.getUser);
+routes.post("/users", userController.createUser);
 
 export  {routes};
